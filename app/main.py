4c1c462d7579696f02e071af6a2da4828808e669
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2025-04-10 19:37:45 +0300

    feat: add image post endpoint

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 import os
+import aiofiles
 
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, UploadFile, status
+from fastapi.responses import Response, FileResponse
 
 
 app = FastAPI()
 
 
 image_path = os.getenv("IMAGE_PATH")
+
+
+@app.put("/coffee/image")
+async def update_coffee_image(file: UploadFile):
+    async with aiofiles.open(image_path, "wb") as image_file:
+        await image_file.write(await file.read())
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.get("/coffee/image")
