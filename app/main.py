4fe7efb02c8eb147cddc36bcd2292df77387cd27
Lari Nurkka <lari.nurkka@gmail.com>
2025-05-02 22:50:36 +0000

    feat: allow more coffee image file types besides png

--- app/main.py
+++ app/main.py
@@ -15,6 +15,11 @@ if not data_dir:
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
+COFFEE_SUBDIR = "coffee"
+coffee_dir = os.path.join(data_dir, COFFEE_SUBDIR)
+if not os.path.exists(coffee_dir):
+    os.mkdir(coffee_dir)
+
 api_key_header = APIKeyHeader(name="X-API-Key")
 api_key_path = os.getenv("API_KEY_FILE")
 if not api_key_path:
@@ -34,6 +39,19 @@ async def update_coffee_image(file: UploadFile, key: str = Security(api_key_head
     authorized = secrets.compare_digest(key, api_key)
     if not authorized:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    ACCEPTED_FILE_TYPES = ["image/jpeg", "image/webp", "image/png"]
+    if not file.content_type in ACCEPTED_FILE_TYPES:
+        return Response(
+            content=f"Bad content type. Accepted types are: {ACCEPTED_FILE_TYPES}",
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+    extension = file.content_type.split("/")[-1]
+
+    for file in os.scandir(path=coffee_dir):
+        os.remove(file)
+
+    image_path = os.path.join(coffee_dir, f"image.{extension}")
     async with aiofiles.open(image_path, "wb") as image_file:
         await image_file.write(await file.read())
     return Response(status_code=status.HTTP_204_NO_CONTENT)
@@ -42,6 +60,11 @@ async def update_coffee_image(file: UploadFile, key: str = Security(api_key_head
 @app.get("/coffee/image", status_code=status.HTTP_200_OK)
 async def get_coffee_image():
     COFFEE_PLACEHOLDER_PATH = "app/coffee-placeholder.png"
-    
-    image_path = COFFEE_PLACEHOLDER_PATH
+
+    image_path = None
+    for file in os.scandir(path=coffee_dir):
+        image_path = file
+    if not image_path:
+        image_path = COFFEE_PLACEHOLDER_PATH
+
     return FileResponse(path=image_path)
