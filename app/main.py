a07e9ab19e6290668145c792bd10bf89f0b92d73
Lari Nurkka <lari.nurkka@gmail.com>
2025-05-02 21:56:09 +0000

    feat: removed IMAGE_PATH env var

--- app/main.py
+++ app/main.py
@@ -9,14 +9,19 @@ from fastapi.security import APIKeyHeader
 app = FastAPI()
 
 
-image_path = os.getenv("IMAGE_PATH")
+data_dir = os.getenv("DATA_DIR")
+if not data_dir:
+    data_dir = "/tmp/data"
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
 api_key_header = APIKeyHeader(name="X-API-Key")
-api_key_path = os.getenv("IMAGE_API_KEY_FILE")
+api_key_path = os.getenv("API_KEY_FILE")
 if not api_key_path:
     api_key_path = "/run/secrets/apikey"
 with open(api_key_path, "r") as key_file:
     # File leaves a trailing newline
-    IMAGE_API_KEY = key_file.read().rstrip('\n')
+    api_key = key_file.read().strip()
 
 
 @app.get("/")
@@ -26,7 +31,7 @@ async def lifesign():
 
 @app.put("/coffee/image")
 async def update_coffee_image(file: UploadFile, key: str = Security(api_key_header)):
-    authorized = secrets.compare_digest(key, IMAGE_API_KEY)
+    authorized = secrets.compare_digest(key, api_key)
     if not authorized:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     async with aiofiles.open(image_path, "wb") as image_file:
@@ -36,4 +41,7 @@ async def update_coffee_image(file: UploadFile, key: str = Security(api_key_head
 
 @app.get("/coffee/image", status_code=status.HTTP_200_OK)
 async def get_coffee_image():
-    return FileResponse(path=image_path, media_type="image/png")
+    COFFEE_PLACEHOLDER_PATH = "app/coffee-placeholder.png"
+    
+    image_path = COFFEE_PLACEHOLDER_PATH
+    return FileResponse(path=image_path)
