c9804b50f8d4ac42de7ad222b124759528540ae4
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2025-05-04 19:48:52 +0300

    test: add test verifying coffee image uploading

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
 import unittest
+from io import BytesIO
 from fastapi import status
 from fastapi.testclient import TestClient
 from app.main import app
-from PIL import Image
+from PIL import Image, ImageChops
 
 
 class TestKattilaApi(unittest.TestCase):
@@ -53,6 +54,18 @@ class TestKattilaApi(unittest.TestCase):
         files = {"file": ("filename", img.tobytes(), "image/example")}
         response = self.client.put("/coffee/image", headers=headers, files=files)
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_coffee_image_endpoint_image_updates(self):
+        sent_image = Image.new("RGB", (600, 800), color="white")
+        sent_image_bytes = BytesIO()
+        sent_image.save(sent_image_bytes, "png")
+        sent_image_bytes.seek(0)
+        headers = {"X-API-Key": "TESTING_API_KEY"}
+        files = {"file": ("filename", sent_image_bytes, "image/png")}
+        self.client.put("/coffee/image", headers=headers, files=files)
+        response = self.client.get("/coffee/image")
+        received_image = Image.open(BytesIO(response.content))
+        self.assertFalse(ImageChops.difference(sent_image, received_image).getbbox())
 
 
 if __name__ == "__main__":
