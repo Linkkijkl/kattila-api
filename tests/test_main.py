27baa19b5b3323d704152058f042492910fdb5b8
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2025-05-05 20:43:42 +0300

    test: refactor test fixtures

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,6 +14,11 @@ class TestKattilaApi(unittest.TestCase):
     def setUp(self) -> None:
         with TestClient(app) as client:
             self.client = client
+            self.testing_headers = {"X-API-Key": "TESTING_API_KEY"}
+            self.test_white_image = Image.new("RGB", (600, 800), color="white")
+            self.test_white_image_bytes_png = BytesIO()
+            self.test_white_image.save(self.test_white_image_bytes_png, "png")
+            self.test_white_image_file = {"file": ("filename", self.test_white_image_bytes_png, "image/png")}
 
     def test_lifesign_endpoint_exists(self):
         response = self.client.get("/")
@@ -24,62 +29,35 @@ class TestKattilaApi(unittest.TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_coffee_image_endpoint_post_not_allowed(self):
-        sent_image = Image.new("RGB", (600, 800), color="white")
-        sent_image_bytes = BytesIO()
-        sent_image.save(sent_image_bytes, "png")
-        headers = {"X-API-Key": "TESTING_API_KEY"}
-        files = {"file": ("filename", sent_image_bytes, "image/png")}
-        response = self.client.post("/coffee/image", headers=headers, files=files)
+        response = self.client.post("/coffee/image", headers=self.testing_headers, files=self.test_white_image_file)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
         self.assertEqual(response.json(), {"detail": "Method Not Allowed"})
 
     def test_coffee_image_endpoint_put_missing_api_key_headers(self):
-        sent_image = Image.new("RGB", (600, 800), color="white")
-        sent_image_bytes = BytesIO()
-        sent_image.save(sent_image_bytes, "png")
-        files = {"file": ("filename", sent_image_bytes, "image/png")}
-        response = self.client.put("/coffee/image", files=files)
+        response = self.client.put("/coffee/image", files=self.test_white_image_file)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(response.json(), {"detail": "Not authenticated"})
 
     def test_coffee_image_endpoint_put_wrong_api_key_headers(self):
-        sent_image = Image.new("RGB", (600, 800), color="white")
-        sent_image_bytes = BytesIO()
-        sent_image.save(sent_image_bytes, "png")
-        headers = {"X-API-Key": "UNAUTHORIZED_KEY"}
-        files = {"file": ("filename", sent_image_bytes, "image/png")}
-        response = self.client.put("/coffee/image", headers=headers, files=files)
+        incorrect_headers = {"X-API-Key": "UNAUTHORIZED_KEY"}
+        response = self.client.put("/coffee/image", headers=incorrect_headers, files=self.test_white_image_file)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.json(), {"detail": "Unauthorized"})
 
     def test_coffee_image_endpoint_put_correct_api_key_headers(self):
-        sent_image = Image.new("RGB", (600, 800), color="white")
-        sent_image_bytes = BytesIO()
-        sent_image.save(sent_image_bytes, "png")
-        headers = {"X-API-Key": "TESTING_API_KEY"}
-        files = {"file": ("filename", sent_image_bytes, "image/png")}
-        response = self.client.put("/coffee/image", headers=headers, files=files)
+        response = self.client.put("/coffee/image", headers=self.testing_headers, files=self.test_white_image_file)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_coffee_image_endpoint_incorrect_media_type(self):
-        sent_image = Image.new("RGB", (600, 800), color="white")
-        sent_image_bytes = BytesIO()
-        sent_image.save(sent_image_bytes, "png")
-        headers = {"X-API-Key": "TESTING_API_KEY"}
-        files = {"file": ("filename", sent_image_bytes, "image/example")}
-        response = self.client.put("/coffee/image", headers=headers, files=files)
+        files = {"file": ("filename", self.test_white_image_bytes_png, "image/example")}
+        response = self.client.put("/coffee/image", headers=self.testing_headers, files=files)
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     def test_coffee_image_endpoint_image_updates(self):
-        sent_image = Image.new("RGB", (600, 800), color="white")
-        sent_image_bytes = BytesIO()
-        sent_image.save(sent_image_bytes, "png")
-        headers = {"X-API-Key": "TESTING_API_KEY"}
-        files = {"file": ("filename", sent_image_bytes, "image/png")}
-        self.client.put("/coffee/image", headers=headers, files=files)
+        self.client.put("/coffee/image", headers=self.testing_headers, files=self.test_white_image_file)
         response = self.client.get("/coffee/image")
         received_image = Image.open(BytesIO(response.content))
-        self.assertFalse(ImageChops.difference(sent_image, received_image).getbbox())
+        self.assertFalse(ImageChops.difference(self.test_white_image, received_image).getbbox())
 
 
 if __name__ == "__main__":
